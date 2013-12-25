a061fd3f587ad07fd2cfeeb2f66e49c7bb0b071d
Eros Akos <erosakos02@gmail.com>
2013-12-25 16:49:32 +0100

    egy része megvan, nem tudom jó e, ezt tudtam kihámozni abból amit írtál

---
+++ TestGame1/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGame1
+{
+    class Block
+    {
+        private string texture;
+        private bool solid;
+        public string Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public bool Solid
+        {
+            get { return solid; }
+            set { solid = value; }
+        }
+
+        static enum BlockType
+        {}
+
+    }
+}
